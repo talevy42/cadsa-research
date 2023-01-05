c052ef04eb9f72d7ef295836488fced6892c9350
Tal Levy <tal@secondspectrum.com>
2023-01-04 22:58:12 -0800

    flake8, la city council mapping, and transform precinct as a script

--- scripts/transform_precinct.py
+++ scripts/transform_precinct.py
@@ -1,9 +1,20 @@
+import sys
 import geopandas as gpd
 import csv
 
+EXPORT_COLS = ["District", "area_col", "Code", "percent"]
+
+
 def convert(precinct_file, district_file, output_file):
-    precincts = gpd.read_file(precinct_file)
-    districts = gpd.read_file(district_file)
+    precincts = gpd.read_file(precinct_file).to_crs("EPSG:4326")
+    districts = gpd.read_file(district_file).to_crs("EPSG:4326")
+    overlay = districts.overlay(precincts, how="intersection")
+    overlay["area_col"] = overlay.area
+    overlay["percent"] = overlay.apply(
+        lambda x: x.area_col / precincts[precincts.Code == x.Code].area.sum(), axis=1
+    )
+    overlay[overlay.percent > 0.1][EXPORT_COLS].to_csv(output_file)
+
 
 if __name__ == "__main__":
     convert(sys.argv[1], sys.argv[2], sys.argv[3])
