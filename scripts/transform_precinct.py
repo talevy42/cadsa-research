64e6efee6dd36154f99ea5115fa856f322f0b1fb
Tal Levy <tal@secondspectrum.com>
2023-01-07 14:20:53 -0800

    mappings for some counties

--- scripts/transform_precinct.py
+++ scripts/transform_precinct.py
@@ -1,20 +1,41 @@
 import sys
 import geopandas as gpd
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 import csv
 
-EXPORT_COLS = ["District", "area_col", "Code", "percent"]
+EXPORT_COLS = ["District", "area_col", "percent", "precinct"]
 
 
-def convert(precinct_file, district_file, output_file):
-    precincts = gpd.read_file(precinct_file).to_crs("EPSG:4326")
-    districts = gpd.read_file(district_file).to_crs("EPSG:4326")
+def convert(args):
+    precincts = gpd.read_file(args.precinct_file).to_crs("EPSG:4326")
+    districts = gpd.read_file(args.district_file).to_crs("EPSG:4326")
+    code_col = args.code_col
     overlay = districts.overlay(precincts, how="intersection")
     overlay["area_col"] = overlay.area
-    overlay["percent"] = overlay.apply(
-        lambda x: x.area_col / precincts[precincts.Code == x.Code].area.sum(), axis=1
-    )
-    overlay[overlay.percent > 0.1][EXPORT_COLS].to_csv(output_file)
+    try:
+        overlay["percent"] = overlay.apply(
+            lambda x: x.area_col / precincts[precincts[code_col] == x[code_col]].area.sum(), axis=1
+        )
+        overlay.rename(columns={code_col: "precinct"}, inplace=True)
+        overlay[overlay.percent > 0.1][EXPORT_COLS].to_csv(args.output_file)
+    except (AttributeError, KeyError) as e:
+        print(f"Valid cols: {list(precincts.columns)}")
+        raise e
 
 
 if __name__ == "__main__":
-    convert(sys.argv[1], sys.argv[2], sys.argv[3])
+    parser = ArgumentParser(
+        description="Translator for taking precinct shapefiles and generating "
+        + "a mapping to new districts",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("precinct_file", type=str, help="Shapefile for precincts")
+    parser.add_argument("district_file", type=str, help="Shapefile for new districts")
+    parser.add_argument(
+        "output_file", default="out.csv", type=str, help="CSV filename for mappings"
+    )
+    parser.add_argument(
+        "-c", "--code-col", type=str, default="Code", help="Column name for precinct id"
+    )
+    args = parser.parse_args()
+    convert(args)
